4fb85a5744b954ef024bfd89a464cc42de73b74f
unknown <redsun@054.(none)>
2012-12-03 17:03:01 +0800

    ff17.0.1 patch fix

--- iceweasel/17/bat_patch.sh
+++ iceweasel/17/bat_patch.sh
@@ -7,7 +7,8 @@ done
 for MY_PATCH in $( ls $FF_PATCH/up/*.patch ); do
         patch -p1 -u < $MY_PATCH
 done
-patch -p1 -u < $FF_PATCH/fpfast.patch
-patch -p1 -u < $FF_PATCH/sqlite3_2.patch
+for MY_PATCH in $( ls $FF_PATCH/fix/*.patch ); do
+        patch -p1 -u < $MY_PATCH
+done
 7z.exe x $FF_PATCH/betterpgo.7z  -o$FF_SRC_ROOT/build/pgo -aoy
 7z.exe x $FF_PATCH/official.7z  -o$FF_SRC_ROOT/browser/branding -aoy
